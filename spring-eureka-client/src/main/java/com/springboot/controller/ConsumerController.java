27c961ff1567c2bbe38fc8c3b9b50217f10cbc96
liqiang <15801718321@163.com>
2019-12-19 18:47:07 +0800

    spring-eureka-client

---
+++ spring-eureka-client/src/main/java/com/springboot/controller/ConsumerController.java
@@ -0,0 +1,33 @@
+package com.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+	
+	@Autowired
+	DiscoveryClient discoveryClient;
+	@GetMapping("/hello")
+	public String hello(String name) {
+		ServiceInstance serviceInstance=discoveryClient.getLocalServiceInstance();
+		serviceInstance.getHost();
+		serviceInstance.getPort();
+		return "hello 1 " + name;
+	}
+
+	@GetMapping("/hello1")
+	public String hello1(@RequestParam String name) {
+		return "hello 1 " + name;
+	}
+
+	@GetMapping("/hello2")
+	public String hello2(@RequestHeader String name, @RequestHeader int age) {
+		return "hello 1 " + name + "  age:" + age;
+	}
+}
